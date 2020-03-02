68a4aa42f622b2b46279db91d0d071a77b8efbf6
Blurring Shadow <976999484@qq.com>
2020-03-02 15:40:00 +0800

    Add Question: longest substring without repeating characters

---
+++ LeetCode-3-Longest-Substring-Without-Repeating-Characters/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * Given a string, find the length of the longest substring without repeating characters.
+ */
+
+#include <array>
+#include <iostream>
+#include <optional>
+
+// ReSharper disable once CppInconsistentNaming
+class Solution
+{
+public:
+    std::array<std::optional<size_t>, std::numeric_limits<char>::max()> char_indices{};
+
+    // ReSharper disable once CppInconsistentNaming
+    int lengthOfLongestSubstring(const std::string_view& s)
+    {
+        size_t begin_index = 0;
+        size_t largest_count = 0;
+
+        {
+            const auto size = s.size();
+
+            for (size_t i = 0; i != size; ++i)
+            {
+                auto& index = char_indices[s[i]];
+                if (index)
+                {
+                    const auto new_begin_index = *index + 1;
+                    for (; begin_index < new_begin_index; ++begin_index)
+                        char_indices[s[begin_index]].reset();
+                }
+                else
+                {
+                    const auto count = i - begin_index + 1;
+                    if (count > largest_count) largest_count = count;
+                }
+
+                index = i;
+            }
+        }
+        return static_cast<int>(largest_count);
+    }
+};
+
+int main()
+{
+    std::cout << Solution{}.lengthOfLongestSubstring("abcabcbb");
+    return 0;
+}
