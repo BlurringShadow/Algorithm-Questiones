7e75113bef3ab359d2947a5cecfcbe13fedb6c46
Blurring Shadow <976999484@qq.com>
2020-08-27 11:50:37 +0800

    [Update] Optimization

--- LeetCode-18-4Sum/main.cpp
+++ LeetCode-18-4Sum/main.cpp
@@ -70,11 +70,13 @@ class Solution
         const size_t current_index_pos
     )
     {
-        const auto last_index = static_cast<size_t>(num_end - num_begin - 1);
+        const auto max_num = *(num_end - 1);
+        const size_t num_size = num_end - num_begin;
+        const auto last_index = static_cast<size_t>(num_size - 1);
         const auto next_index_pos = current_index_pos + 1;
         auto& current_index = *(indices_begin + current_index_pos);
         const size_t indices_size = indices_end - indices_begin;
-        vector<vector<int>> result_set;
+        vector<int> result;
 
         while(true)
         {
@@ -82,9 +84,11 @@ class Solution
                 std::upper_bound(num_begin + current_index + 1, num_end, *(num_begin + current_index)) - num_begin;
 
             // check if available index count is larger than current pos
-            if(last_index - current_index <= current_index_pos) break;
+            if(current_index > last_index || last_index - current_index <= current_index_pos) break;
 
-            if(!result_set.empty()) return tuple{true, result_set};
+            sum_cache[current_index_pos] =
+                (next_index_pos < indices_size ? sum_cache[next_index_pos] : 0) + *(num_begin + current_index);
+            if(!result.empty()) return tuple{true, result};
 
             // check possible largest sum is larger than target
             for(auto i = decltype(current_index_pos){0}, num_index = last_index - 1;
@@ -92,27 +96,25 @@ class Solution
                 ++i, --num_index)
                 *(indices_begin + i) = num_index;
 
-            sum_cache[current_index_pos] =
-                (next_index_pos < indices_size ? sum_cache[next_index_pos] : 0) + *(num_begin + current_index);
             for(auto i = current_index_pos; i != 0; --i)
                 sum_cache[i - 1] =
                     sum_cache[i] + *(num_begin + *(indices_begin + i - 1));
 
-            const auto largest_sum = sum_cache[0] + *(num_end - 1);
+            const auto largest_sum = sum_cache[0] + max_num;
             if(largest_sum == sum_target)
             {
-                result_set.emplace_back(vector<int>(SumSize));
+                result.resize(SumSize, max_num);
                 std::transform(
                     std::make_reverse_iterator(indices_end),
                     std::make_reverse_iterator(indices_begin),
-                    result_set.back().begin(),
+                    result.begin(),
                     [&](const auto index) { return *(num_begin + index); }
                 );
             }
-            if(largest_sum > sum_target) return tuple{true, result_set};
+            else if(largest_sum > sum_target) return tuple{true, result};
         }
 
-        return tuple{false, result_set};
+        return tuple{false, result};
     }
 
     template<size_t SumSize>
@@ -165,7 +167,7 @@ class Solution
             for(last_increase_pos = jump ? last_increase_pos + 1 : 0; ; ++last_increase_pos)
             {
                 if(last_increase_pos == indices.size()) return result_set;
-                auto&& [is_accepted, results] = increase_index<SumSize>(
+                auto&& [is_accepted, result] = increase_index<SumSize>(
                     sum_target,
                     num_begin,
                     num_end,
@@ -174,7 +176,7 @@ class Solution
                     indices_end,
                     last_increase_pos
                 );
-                for(auto&& result : results) result_set.emplace_back(std::move(result));
+                if(!result.empty()) result_set.emplace_back(std::move(result));
                 if(is_accepted) break;
             }
         }
@@ -201,15 +203,29 @@ int main() noexcept
 {
     try
     {
-        vector<int> test_data = {-2, -1, 0, 0, 1, 2};
+        vector<tuple<vector<int>, int>> test_data = {
+            /*
+            {{0, 0, 0, 0}, 1},
+            {{-1, -2, 0, 0, 1, 2}, 0},
+            {{-3, -2, -1, 0, 1, 2, 3}, 0},
+            */
+            {{-3, -2, -1, 0, 0, 1, 2, 3}, 0}
+        };
         string output;
 
-        for(const auto& result : Solution::fourSum(test_data, 0))
+        for(auto&& [nums, target] : test_data)
         {
-            for(const auto value : result) output += std::to_string(value) + '\t';
+            output += "input: {";
+            for(const auto num : nums) { output += std::to_string(num) + ','; }
+            output.resize(output.size() - 1);
+            output += "}\ttarget: " + std::to_string(target) + "\nresults:\n";
+            for(const auto& result : Solution::fourSum(nums, target))
+            {
+                for(const auto value : result) output += std::to_string(value) + '\t';
+                output += '\n';
+            }
             output += '\n';
         }
-
         cout << output;
     }
     catch(const std::exception& e) { cout << e.what(); }
