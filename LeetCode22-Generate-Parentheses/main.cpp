1b36b221644a3af32b77dc01ed7428b4a14c3a90
Blurring Shadow <976999484@qq.com>
2020-09-14 11:44:11 +0800

    [Add] generate parenthesis impl

--- LeetCode22-Generate-Parentheses/main.cpp
+++ LeetCode22-Generate-Parentheses/main.cpp
@@ -2,11 +2,7 @@
  * https://leetcode.com/problems/generate-parentheses/
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  * For example, given n = 3, a solution set is:
- * "((()))",
- * "(()())",
- * "(())()",
- * "()(())",
- * "()()()"
+ * "((()))", "(()())", "(())()", "()(())", "()()()"
 */
 #define var decltype(auto)
 #define val const decltype(auto)
@@ -32,11 +28,32 @@ class Solution
         return 0;
     }();
 
+    inline static vector<vector<string>> generated_ = {{""}};
+
+    // see "catalan number" for more detail
+    static void generate_to(const size_t n)
+    {
+        if(n + 1 <= generated_.size()) return;
+        const auto old_size = generated_.size();
+        generated_.resize(n + 1);
+
+        for(auto i = old_size; i < generated_.size(); ++i)
+            for(size_t j = 0; j < i; ++j)
+                for(const auto& s1 : generated_[j])
+                    for(const auto& s2 : generated_[i - 1 - j])
+                    {
+                        auto str = '(' + s1;
+                        str += ')' + s2;
+                        generated_[i].emplace_back(std::move(str));
+                    }
+    }
+
 public:
     // ReSharper disable once CppInconsistentNaming
-    static vector<string> generateParenthesis(const int n)
+    static const vector<string>& generateParenthesis(const int n)
     {
-        return {};
+        generate_to(n);
+        return generated_[n];
     }
 };
 
@@ -44,14 +61,12 @@ int main() noexcept
 {
     try
     {
-        vector<int> test_data
-        {
-        };
+        vector<int> test_data{3, 4, 5};
         string output;
 
         for(const auto v : test_data)
         {
-            output += "num: " + std::to_string(v) + "\nresult: ";
+            output += "num: " + std::to_string(v) + "\nresult: \n";
             for(const auto& str : Solution::generateParenthesis(v)) output += str + '\n';
             output += "\n\n";
         }
