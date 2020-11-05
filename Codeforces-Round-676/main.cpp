9e1073f9800c80f1e8e486a843ff2c5894828368
Blurring Shadow <976999484@qq.com>
2020-11-05 17:17:19 +0800

    [Add] palindromifier

--- Codeforces-Round-676/main.cpp
+++ Codeforces-Round-676/main.cpp
@@ -1,34 +1,13 @@
-#include "putting_bricks_in_the_wall.h"
+#include "palindromifier.h"
 
 int main()
 {
     if constexpr(is_debug)
     {
-        putting_bricks_in_the_wall::create_from_input(
-            std::istringstream{
-                "4\n\
-                 3\n\
-                 S11\n\
-                 011\n\
-                 00F\
-                 4\n\
-                 S010\n\
-                 0001\n\
-                 1000\n\
-                 111F\n\
-                 3\n\
-                 S10\n\
-                 101\n\
-                 01F\n\
-                 5\n\
-                 S0101\n\
-                 00000\n\
-                 01111\n\
-                 11111\n\
-                 0001F\n"
-            }
-        );
+        palindromifier::create_from_input(std::istringstream{"abac"});
+        palindromifier::create_from_input(std::istringstream{"acccc"});
+        palindromifier::create_from_input(std::istringstream{"hannah"});
     }
-    else putting_bricks_in_the_wall::create_from_input(cin);
+    else palindromifier::create_from_input(cin);
     return 0;
 }
---
+++ Codeforces-Round-676/palindromifier.h
@@ -0,0 +1,58 @@
+﻿// Created by BlurringShadow at 2020-11-05-下午 4:28
+
+#pragma once
+#include "../algorithm_utility/utils.h"
+
+/*
+ * Ringo found a string s of length n in his yellow submarine.
+ * The string contains only lowercase letters from the English alphabet.
+ * As Ringo and his friends love palindromes,
+ * he would like to turn the string s into a palindrome by applying two types of operations to the string.
+ *
+ * The first operation allows him to choose i (2≤i≤n−1)
+ * and to append the substring s2s3…si (i−1 characters) reversed to the front of s.
+ * The second operation allows him to choose i (2≤i≤n−1)
+ * and to append the substring sisi+1…sn−1 (n−i characters) reversed to the end of s.
+ * Note that characters in the string in this problem are indexed from 1.
+ *
+ * For example suppose s=abcdef.
+ * If he performs the first operation with i=3 then he appends cb to the front of s and the result will be cbabcdef.
+ * Performing the second operation on the resulted string with i=5 will yield cbabcdefedc.
+ * 
+ * Your task is to help Ringo make the entire string a palindrome
+ * by applying any of the two operations (in total) at most 30 times.
+ * The length of the resulting palindrome must not exceed 106
+ * 
+ * It is guaranteed that under these constraints there always is a solution.
+ * Also note you do not have to minimize neither the number of operations applied,
+ * nor the length of the resulting string,
+ * but they have to fit into the constraints.
+ * 
+ * Input
+ * The only line contains the string S (3≤|s|≤105) of lowercase letters from the English alphabet.
+ * 
+ * Output
+ * The first line should contain k(0≤k≤30), the number of operations performed.
+ * 
+ * Each of the following k lines should describe an operation in form L i or R i.
+ * L represents the first operation, R represents the second operation, i represents the index chosen.
+ * 
+ * The length of the resulting palindrome must not exceed 106.
+ */
+class palindromifier
+{
+public:
+    const size_t size;
+
+    explicit constexpr palindromifier(const size_t s) noexcept : size(s) {}
+
+    void solve() const { cout << "3\nR " << size - 1 << "\nL " << size << "\nL " << 2; }
+
+    template<typename InputStream
+#ifdef __cpp_lib_concepts
+    > requires requires(InputStream&& is) { get_from_stream<int>(is); }
+#else
+    , SFINAE((std::is_base_of_v<std::istream, std::remove_reference_t<InputStream>>))>
+#endif
+    static void create_from_input(InputStream&& is) { palindromifier{get_from_stream<string>(is).size()}.solve(); }
+};
