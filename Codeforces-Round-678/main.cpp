1761b921c799fa4b3c53337fdf86af073c020a13
Blurring Shadow <976999484@qq.com>
2020-11-16 10:14:29 +0800

    [Refactor] round 678 reorder impl refactor

--- Codeforces-Round-678/main.cpp
+++ Codeforces-Round-678/main.cpp
@@ -1,51 +1,15 @@
-#include <iostream>
-
-using std::cout;
-using std::cin;
-
-/*
- * For a given array a consisting of n integers and a given integer m.
- * Find if it is possible to reorder elements of the array a in such a way that n∑i=1 n∑j=i a[j]/j equals m.
- * It is forbidden to delete elements as well as insert new elements.
- * Please note that no rounding occurs during division, for example, 5/2=2.5.
- * Input
- * The first line contains a single integer t — the number of test cases (1≤t≤100).
- * The test cases follow, each in two lines.
- * 
- * The first line of a test case contains two integers n and m (1≤n≤100, 0≤m≤106).
- * The second line contains integers a1,a2,…,an (0≤ai≤106) — the elements of the array.
- * 
- * Output
- * For each test case print "YES",
- * if it is possible to reorder the elements of the array in such a way
- * that the given formula gives the given value, and "NO" otherwise.
- *  */
-class Reorder
-{
-public:
-
-    static void create_from_input()
-    {
-        unsigned test_case_count;
-        
-        for(cin >> test_case_count; test_case_count > 0; --test_case_count)
-        {
-            size_t array_size, target, sum = 0;
-            cin >> array_size >> target;
-            for(; array_size > 0; --array_size)
-            {
-                size_t num;
-                cin >> num;
-                sum += num;
-            }
-
-            cout << (sum == target ? "YES" : "NO") << '\n';
-        }
-    }
-};
+#include "reorder.h"
 
 int main()
 {
-    Reorder::create_from_input();
+    if constexpr(is_debug)
+        reorder::create_from_input(
+            std::istringstream{
+                "2\
+                 3 8    2 5 1\
+                 4 4    0 1 2 3"
+            }
+        );
+    else reorder::create_from_input(cin);
     return 0;
 }
---
+++ Codeforces-Round-678/reorder.h
@@ -0,0 +1,43 @@
+﻿// Created by BlurringShadow at 2020-11-16-上午 9:48
+
+#pragma once
+#include "../algorithm_utility/utils.h"
+
+/*
+ * For a given array a consisting of n integers and a given integer m.
+ * Find if it is possible to reorder elements of the array a in such a way that n∑i=1 n∑j=i a[j]/j equals m.
+ * It is forbidden to delete elements as well as insert new elements.
+ * Please note that no rounding occurs during division, for example, 5/2=2.5.
+ * Input
+ * The first line contains a single integer t — the number of test cases (1≤t≤100).
+ * The test cases follow, each in two lines.
+ * 
+ * The first line of a test case contains two integers n and m (1≤n≤100, 0≤m≤106).
+ * The second line contains integers a1,a2,…,an (0≤ai≤106) — the elements of the array.
+ * 
+ * Output
+ * For each test case print "YES",
+ * if it is possible to reorder the elements of the array in such a way
+ * that the given formula gives the given value, and "NO" otherwise.
+ *  */
+class reorder
+{
+public:
+    template<typename InputStream
+#ifdef __cpp_lib_concepts
+    > requires requires(InputStream&& is) { get_from_stream<int>(is); }
+#else
+        // ReSharper disable once CppRedundantParentheses
+        , SFINAE((std::is_base_of_v<std::istream, std::remove_reference_t<InputStream>>))>
+#endif
+    static void create_from_input(InputStream&& is)
+    {        
+        for(auto test_case_count = get_from_stream<unsigned>(is); test_case_count > 0; --test_case_count)
+        {
+            auto array_size = get_from_stream<size_t>(is);
+            auto target = get_from_stream<size_t>(is);
+            for(; array_size > 0; --array_size) target -= get_from_stream<size_t>(is);
+            cout << (target == 0 ? "YES" : "NO") << '\n';
+        }
+    }
+};
