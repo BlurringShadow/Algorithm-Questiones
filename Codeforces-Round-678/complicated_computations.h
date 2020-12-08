2fffbef5c3479838b6b2c81aeb52b3ceb832bf1e
Blurring Shadow <976999484@qq.com>
2020-12-08 12:55:51 +0800

    [Add] complicated_computations

---
+++ Codeforces-Round-678/complicated_computations.h
@@ -0,0 +1,140 @@
+﻿// Created by BlurringShadow at 2020-11-18-上午 9:24
+
+#pragma once
+
+#include "../algorithm_utility/utils.h"
+
+/*
+ * In this problem MEX of a certain array is the smallest positive integer not contained in this array.
+
+ * Everyone knows this definition, including Lesha.
+ * But Lesha loves MEX, so he comes up with a new problem involving MEX every day, including today.
+ * 
+ * You are given an array a of length n.
+ * Lesha considers all the non-empty subarrays of the initial array and computes MEX for each of them.
+ * Then Lesha computes MEX of the obtained numbers.
+ * 
+ * An array b is a subarray of an array a,
+ * if b can be obtained from a by deletion of several (possible none or all) elements from the beginning and
+ * several (possibly none or all) elements from the end.
+ * In particular, an array is a subarray of itself.
+ * 
+ * Lesha understands that the problem is very interesting this time,
+ * but he doesn't know how to solve it.
+ * Help him and find the MEX of MEXes of all the subarrays!
+ * 
+ * Input
+ * The first line contains a single integer n (1≤n≤105) — the length of the array.
+ * 
+ * The next line contains n integers a1,a2,…,an (1≤ai≤n) — the elements of the array.
+ * 
+ * Output
+ * Print a single integer — the MEX of MEXes of all subarrays.
+ */
+class complicated_computations final
+{
+public:
+    using uint = std::uint32_t;
+
+    static constexpr std::pair<uint, uint> value_range = {1, static_cast<uint>(1e5)};
+
+private:
+    struct backward_linked_num
+    {
+        uint num = std::numeric_limits<uint>::max();
+        uint next_pos = std::numeric_limits<uint>::max();
+    };
+
+    static uint comparer(const uint left, const uint right) { return std::max(left, right); }
+
+    vector<backward_linked_num> nums_{};
+
+    binary_indexed_tree<uint, decltype(&comparer)> positions_;
+
+public:
+    explicit complicated_computations(const vector<uint>& nums) noexcept : nums_(nums.size()),
+        positions_(
+            &comparer,
+            [this, &nums]
+            {
+                const auto size = nums.size();
+                vector<uint> next_pos(size, auto_cast{size});
+
+                for(auto i = nums.size() - 1; i < size; --i)
+                {
+                    const auto num = nums[i];
+                    nums_[i] = {num, next_pos[num - 1]};
+                    next_pos[num - 1] = auto_cast{i};
+                }
+
+                return next_pos;
+            }()
+        ) {}
+
+    void solve()
+    {
+        const uint num_size = auto_cast{nums_.size()};
+
+        if(num_size == 1)
+        {
+            cout << std::min<uint>(nums_.front().num, 2);
+            return;
+        }
+
+        const auto candidate_size = num_size + 1;
+        vector<bool> candidates(candidate_size);
+
+        {
+            auto candidate_index = num_size;
+
+            for(auto pos = num_size; candidate_index > 0; --candidate_index, pos = positions_[candidate_index])
+            {
+                if(positions_.get_range(candidate_index - 1) < pos) candidates[candidate_index] = true;
+                if(pos != num_size)
+                {
+                    candidates.front() = true;
+                    --candidate_index;
+                    break;
+                }
+            }
+
+            for(; candidate_index > 0; --candidate_index)
+                if(positions_.get_range(candidate_index - 1) < positions_[candidate_index])
+                    candidates[candidate_index] = true;
+        }
+
+        for(uint i = 0; i < num_size; ++i)
+        {
+            const auto [num, next_pos] = nums_[i];
+            auto&& candidate_flag = candidates[num - 1];
+
+            positions_.update(num - 1, next_pos);
+
+            if(!candidate_flag && is_between(num, {2}, static_cast<uint>(candidates.size())))
+            {
+                // [i, max_pos] contains the all num from 1 to num - 1
+                const auto max_pos = positions_.get_range(num - 2);
+
+                if(max_pos < next_pos) candidate_flag = true;
+                else if(max_pos == num_size) candidates.resize(max_pos);
+            }
+        }
+
+        const auto& cbegin = candidates.cbegin();
+        cout << std::find(cbegin, candidates.cend(), false) - cbegin + 1 << '\n';
+    }
+
+    template<typename InputStream
+#ifdef __cpp_lib_concepts
+    > requires requires(InputStream&& is) { get_from_stream<int>(is); }
+#else
+    , SFINAE((std::is_base_of_v<std::istream, std::remove_reference_t<InputStream>>))>
+#endif
+    static void create_from_input(InputStream&& is)
+    {
+        vector<uint> nums(get_from_stream<uint>(is));
+        for(auto& num : nums) is >> num;
+
+        complicated_computations{nums}.solve();
+    }
+};
--- Codeforces-Round-678/main.cpp
+++ Codeforces-Round-678/main.cpp
@@ -1,28 +1,31 @@
-#include "bandit_in_a_city.h"
+#include "complicated_computations.h"
 
 int main()
 {
     if constexpr(is_debug)
     {
-        bandit_in_a_city::create_from_input(std::istringstream{"3 1 1 3 1 2"});
-        bandit_in_a_city::create_from_input(std::istringstream{"3 1 1 3 1 3"});
-        bandit_in_a_city::create_from_input(std::istringstream{"3 1 1 2 0 0"});
-        bandit_in_a_city::create_from_input(std::istringstream{"5 1 1 1 4 28 0 0 0 0"});
-        bandit_in_a_city::create_from_input(
+        complicated_computations::create_from_input(std::istringstream{"3 1 2 3"}); //5 
+        complicated_computations::create_from_input(std::istringstream{"3 1 3 2"}); //3 
+        complicated_computations::create_from_input(std::istringstream{"5 1 1 1 1 1"}); //1
+        complicated_computations::create_from_input(std::istringstream{"5 1 4 3 1 2"}); //6
+        complicated_computations::create_from_input(
             std::istringstream{
-                "10\
-                 1 2 3 1 3 4 2 4 6\
-                 68 5 44 83 46 92 32 51 2 89"
+                "51 45 37 1 8 4 41 35 15 17 2 7 48 20 42 41 5 26 25 37 1 19 43 3 49 33 2 45 43 39 14 1 21 22 17 18 8 49 24 35 26 22 43 45 1 3 17 1 16 35 33 5"
             }
-        );
-        bandit_in_a_city::create_from_input(
+        ); //5
+        complicated_computations::create_from_input(
             std::istringstream{
-                "5\
-                 1 2 2 1\
-                 5 5 5 5 5"
+                "100 100 2 3 99 5 6 7 8 9 10 65 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 48 32 83 79 35 36 37 38 39 40 41 42 43 44 45 46 47 31 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 11 66 67 68 69 70 71 72 73 80 75 76 77 78 34 74 81 82 33 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 4 1"
             }
-        );
+        ); //3
+        {
+            static constexpr auto max = complicated_computations::value_range.second;
+            std::stringstream is;
+            is << max;
+            for(complicated_computations::uint i = 0; i < max; ++i)  
+                is << ' ' << i + 1;
+            complicated_computations::create_from_input(is);
+        }
     }
-    else bandit_in_a_city::create_from_input(cin);
-    return 0;
+    else complicated_computations::create_from_input(cin);
 }
