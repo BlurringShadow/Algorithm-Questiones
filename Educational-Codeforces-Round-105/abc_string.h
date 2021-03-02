5ea65bba9daf1c67eb60f25e2fbbd62b6df65bcd
Blurring Shadow <976999484@qq.com>
2021-03-02 23:50:38 +0800

    round-105 abc_string

---
+++ Educational-Codeforces-Round-105/abc_string.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+#include "../algorithm_utility/utils.h"
+
+#include <bitset>
+#include <list>
+
+class abc_string final
+{
+    enum class abc { a, b, c };
+
+    std::vector<abc> str_;
+
+    [[nodiscard]] auto solve() const
+    {
+        using namespace std;
+
+        for(uint8_t i = 1; i < 7; ++i)
+        {
+            std::bitset<3> map{i};
+
+            std::list<bool> str_list(str_.size());
+            std::transform(
+                str_.cbegin(),
+                str_.cend(),
+                str_list.begin(),
+                [&](const auto value)
+                {
+                    return map[static_cast<std::underlying_type_t<abc>>(value)];
+                }
+            );
+
+            for(auto&& it = str_list.cbegin();;)
+            {
+                if(str_list.empty()) 
+                    return true;
+
+                it = adjacent_find(it, str_list.cend(), std::less<>{});
+                if(it == str_list.cend()) break;
+
+                it = str_list.erase(it, next(it, 2));
+                if(it != str_list.cbegin()) std::advance(it, -1);
+            }
+        }
+
+        return false;
+    }
+
+public:
+    friend std::ostream& operator<<(std::ostream& os, abc_string& s)
+    {
+        return os << (s.solve() ? "YES" : "NO") << '\n';
+    }
+
+    friend std::istream& operator>>(std::istream& is, abc_string& v)
+    {
+        string&& input_str = get_from_stream(is);
+        auto& str = v.str_;
+        str.resize(input_str.size());
+        std::transform(
+            input_str.cbegin(),
+            input_str.cend(),
+            str.begin(),
+            [](const char c)
+            {
+                switch(c)
+                {
+                case 'A': return abc::a;
+                    break;
+                case 'B': return abc::b;
+                    break;
+                case 'C': return abc::c;
+                    break;
+                }
+                throw std::invalid_argument("");
+            }
+        );
+        return is;
+    }
+};
+
+using problem_t = abc_string;
---
+++ Educational-Codeforces-Round-105/main.cpp
@@ -0,0 +1,33 @@
+﻿// Created by BlurringShadow at 2021-03-02-下午 10:50
+#include "abc_string.h"
+
+int main()
+{
+    using namespace std;
+
+    problem_t problem;
+    if constexpr(is_debug)
+    {
+        istringstream is;
+        for(auto str : {
+                "AABBAC",
+                "CACA",
+                "BBBBAC",
+                "ABCA"
+            })
+        {
+            is.str(str);
+            is.seekg(0);
+            is >> problem;
+
+            cout << problem;
+        }
+    }
+    else //for(auto i = get_from_stream<size_t>(cin); i > 0; --i)
+    {
+        cin >> problem;
+        cout << problem;
+    }
+
+    return 0;
+}
