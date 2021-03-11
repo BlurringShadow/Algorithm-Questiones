9fec24f56aa98845462073e3c710ad2ebc70afd1
Blurring Shadow <976999484@qq.com>
2021-03-11 21:46:10 +0800

    round 700 the great hero

--- Codeforces-Round-700/main.cpp
+++ Codeforces-Round-700/main.cpp
@@ -1,19 +1,21 @@
 ﻿// Created by BlurringShadow at 2021-02-07-下午 10:50
 
-#include "yet_another_string_game.h"
+#include "the_great_hero.h"
 
 int main()
 {
     using namespace std;
 
-    yet_another_string_game problem;
+    problem_t problem;
     if constexpr(is_debug)
     {
         istringstream is;
         for(auto str : {
-                "a",
-                "bbbb",
-                "az"
+                "3 17\n1\n2\n16\n",
+                "10 999\n3\n10 20 30\n100 50 30\n",
+                "1000 1000\n4\n200 300 400 500\n1000 1000 1000 1000",
+                "999 999\n1\n1000\n1000",
+                "999 999\n1\n1000000\n999"
             })
         {
             is.str(str);
---
+++ Codeforces-Round-700/the_great_hero.h
@@ -0,0 +1,67 @@
+﻿// Created by BlurringShadow at 2021-03-11-下午 9:02
+
+
+#pragma once
+
+#include "../algorithm_utility/utils.h"
+
+#include <numeric>
+
+class the_great_hero final
+{
+    struct property
+    {
+        uint64_t attack{};
+        uint64_t health{};
+    };
+
+    property hero_prop_;
+    vector<property> monsters_prop_;
+
+    [[nodiscard]] auto solve() const
+    {
+        using namespace std;
+
+        uint64_t max_single_damage = 0;
+        const auto& costs = [&, this]
+        {
+            vector<uint64_t> vec(monsters_prop_.size());
+
+            for(size_t i = 0; i < vec.size(); ++i)
+            {
+                const auto& monster = monsters_prop_[i];
+                vec[i] = (monster.health + hero_prop_.attack - 1) / 
+                    hero_prop_.attack * monster.attack;
+
+                set_if_greater(max_single_damage, monsters_prop_[i].attack);
+            }
+
+            return vec;
+        }();
+        return hero_prop_.health + max_single_damage >
+            std::accumulate(costs.cbegin(), costs.cend(), uint64_t{});
+    }
+
+public:
+    friend std::ostream& operator<<(std::ostream& os, the_great_hero& s)
+    {
+        return os << (s.solve() ? "YES" : "NO") << '\n';
+    }
+
+    friend std::istream& operator>>(std::istream& is, the_great_hero& v)
+    {
+        {
+            auto& hero = v.hero_prop_;
+            is >> hero.attack >> hero.health;
+        }
+        {
+            auto& monsters = v.monsters_prop_;
+            monsters.resize(get_from_stream(is));
+            for(auto& prop : monsters) is >> prop.attack;
+            for(auto& prop : monsters) is >> prop.health;
+        }
+        return is;
+    }
+};
+
+using problem_t = the_great_hero;
