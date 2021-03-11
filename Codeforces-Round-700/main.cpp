5ad408b7392e930ed5f50c05fd9e8b8dd95cf52c
Blurring Shadow <976999484@qq.com>
2021-03-11 22:36:15 +0800

    round 700 searching local minimum

--- Codeforces-Round-700/main.cpp
+++ Codeforces-Round-700/main.cpp
@@ -1,22 +1,17 @@
 ﻿// Created by BlurringShadow at 2021-02-07-下午 10:50
 
-#include "the_great_hero.h"
+#include "searching_local_minimum.h"
 
 int main()
 {
     using namespace std;
 
     problem_t problem;
+    /*
     if constexpr(is_debug)
     {
         istringstream is;
-        for(auto str : {
-                "3 17\n1\n2\n16\n",
-                "10 999\n3\n10 20 30\n100 50 30\n",
-                "1000 1000\n4\n200 300 400 500\n1000 1000 1000 1000",
-                "999 999\n1\n1000\n1000",
-                "999 999\n1\n1000000\n999"
-            })
+        for(auto str : {"5 3 2 1 4 5"})
         {
             is.str(str);
             is.seekg(0);
@@ -25,12 +20,12 @@ int main()
             cout << problem;
         }
     }
-    else
-        for(auto i = get_from_stream<size_t>(cin); i > 0; --i)
-        {
-            cin >> problem;
-            cout << problem;
-        }
+    else //for(auto i = get_from_stream<size_t>(cin); i > 0; --i)
+    */
+    {
+        cin >> problem;
+        cout << problem;
+    }
 
     return 0;
 }
---
+++ Codeforces-Round-700/searching_local_minimum.h
@@ -0,0 +1,50 @@
+﻿// Created by BlurringShadow at 2021-03-11-下午 10:08
+
+#pragma once
+
+#include "../algorithm_utility/utils.h"
+
+class searching_local_minimum final
+{
+    std::istream* is_;
+
+    uint32_t size_;
+
+    [[nodiscard]] uint32_t query(const uint32_t i) const
+    {
+        if(i > 0 && i <= size_)
+        {
+            (cout << "? " << i << '\n').flush();
+            return get_from_stream(*is_);
+        }
+        return size_ + 1;
+    }
+ 
+
+    [[nodiscard]] auto solve() const
+    {
+        using namespace std;
+        uint32_t l = 1, r = size_;
+        while(l != r)
+        {
+            const auto m = (l + r) / 2;
+            if(query(m) < query(m + 1)) r = m;
+            else l = m + 1;
+        }
+        return "! " + std::to_string(l);
+    }
+
+public:
+    friend std::ostream& operator<<(std::ostream& os, searching_local_minimum& s)
+    {
+        return os << s.solve() << '\n';
+    }
+
+    friend std::istream& operator>>(std::istream& is, searching_local_minimum& v)
+    {
+        v.is_ = &is;
+        return is >> v.size_;
+    }
+};
+
+using problem_t = searching_local_minimum;
